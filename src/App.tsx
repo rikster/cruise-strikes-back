8c0f8d75620c9c71668b331cf3bdda7624576ceb
Richard Hounslow <rhounslow@gmail.com>
2023-05-25 15:22:33 +1000

    add apollo client and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://tmdb-one-blue.vercel.app/",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client}>
       <h1 className="text-3xl font-bold underline">
         John Johnsons Cruisey Movie Monday
       </h1>
-    </div>
+    </ApolloProvider>
   );
 }
 
