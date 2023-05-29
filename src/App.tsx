f8345c75dd917d25d33f9fcd53a6cb2066516eb9
Richard Hounslow <rhounslow@gmail.com>
2023-05-29 13:03:38 +1000

    fix outer responsiveness (inner could be better)

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="min-h-screen bg-gray-200 flex items-center justify-center">
-        <div className="bg-white p-10 rounded-lg shadow-2xl w-3/4 md:w-1/2 lg:w-1/3">
+        <div className="bg-white p-10 rounded-lg shadow-2xl w-5/6 md:w-4/5 lg:w-4/5 xl:w-3/4 xxl:w-3/4">
           <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 text-gray-700 text-center">
             John Johnsons Cruisey Movie Monday
           </h1>
