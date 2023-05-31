500c0b84b7763efd3fa0142e227214ddfa1a24af
Richard Hounslow <rhounslow@gmail.com>
2023-05-31 14:44:23 +1000

    update type comments

--- src/queries/types.ts
+++ src/queries/types.ts
@@ -1,12 +1,20 @@
-// This types file is not exhaustive,
+// This types file is not exhaustive
+// (normally generated via server code and schema),
 // it contains the types that are used in the queries folder
 // and for the purpose of the search and listing components and tests
+
+//note Cast in Movie = Cast
 export type Movie = {
   id: string;
   title: string;
   overview: string;
   release_date: string;
   vote_average: number;
+  // credits {
+  //   cast {
+  //     id
+  //     name
+  //   }
 };
 
 export type Movies = Movie[];
